40996556be1581eaba21620d17c3d7268b5151db
mehmetgencv <mehmetgencv@gmail.com>
2023-11-22 01:44:12 +0300

    first commit

---
+++ spring-mongo-rest-api/src/main/java/org/mehmetgenc/Application.java
@@ -0,0 +1,10 @@
+package org.mehmetgenc;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ spring-mongo-rest-api/src/main/java/org/mehmetgenc/entity/User.java
@@ -0,0 +1,4 @@
+package org.mehmetgenc.entity;
+
+public class User {
+}
