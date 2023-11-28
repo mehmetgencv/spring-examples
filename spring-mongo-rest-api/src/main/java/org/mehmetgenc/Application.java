eb55e557c0d5ac839fdbae4541f3c3daa8927aaf
mehmetgencv <mehmetgencv@gmail.com>
2023-11-28 21:59:59 +0300

    add

--- spring-mongo-rest-api/src/main/java/org/mehmetgenc/Application.java
+++ spring-mongo-rest-api/src/main/java/org/mehmetgenc/Application.java
@@ -1,10 +1,14 @@
 package org.mehmetgenc;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(Application.class, args);
     }
 }
--- spring-mongo-rest-api/src/main/java/org/mehmetgenc/entity/User.java
+++ spring-mongo-rest-api/src/main/java/org/mehmetgenc/entity/User.java
@@ -1,4 +1,76 @@
 package org.mehmetgenc.entity;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.HashMap;
+
+@Document
 public class User {
+    @Id
+    private String id;
+    private String name;
+    private String surname;
+    private String email;
+    private String password;
+    private HashMap role;
+
+    public User() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public HashMap getRole() {
+        return role;
+    }
+
+    public void setRole(HashMap role) {
+        this.role = role;
+    }
+
+    public User(String name, String surname, String email, String password, HashMap role) {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
 }
