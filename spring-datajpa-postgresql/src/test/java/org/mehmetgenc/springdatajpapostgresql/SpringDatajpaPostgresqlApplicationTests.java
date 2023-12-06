90a4578813ad67958a0b335ada94e09dc305183a
mehmetgencv <mehmetgencv@gmail.com>
2023-12-06 14:51:11 +0300

    add dependencies

---
+++ spring-datajpa-postgresql/src/test/java/org/mehmetgenc/springdatajpapostgresql/SpringDatajpaPostgresqlApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mehmetgenc.springdatajpapostgresql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDatajpaPostgresqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
